eb7e814102444481c984322b41543adfa80176da
Lmacconnell2 <mac23068@byui.edu>
2026-03-11 10:36:10 -0600

    added frontend skeleton

---
+++ client/src/components/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+	let count = $state(0);
+</script>
+
+<button onclick={() => count++}>
+	Count is {count}
+</button>
+
+<style>
+	button {
+		padding: 0.5rem 1rem;
+		font-size: 1rem;
+	}
+</style>
